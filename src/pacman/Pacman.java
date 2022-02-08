194d78d97c768968e7b64745b183bef66680e2cd
Robson Vieira <robson.ppmg@gmail.com>
2022-02-07 21:38:45 -0300

    jogo do pacman usando Java

---
+++ src/pacman/Pacman.java
@@ -0,0 +1,27 @@
+package pacman;
+
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+
+
+    public Pacman(){
+        add(new Model());
+    }
+
+    public static void main(String[] args) {
+
+        Pacman pacman = new Pacman();
+        pacman.setVisible(true);
+        pacman.setTitle("Pacman");
+        pacman.setSize(380, 420);
+        pacman.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pacman.setLocationRelativeTo(null);
+
+        System.setProperty("sun.java2d.opengl", "true");
+
+
+    }
+
+}
